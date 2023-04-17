8376823a20e61234d37604b25c70c334215cd06a
Pedro Resende <951377+pedroresende@users.noreply.github.com>
2023-04-17 14:56:50 +0100

    Initial Commit

---
+++ next-app/next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: ["@pedroresende/css-error"],
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
---
+++ packages/react-library/dist/components/stencil-generated/index.js
@@ -0,0 +1,5 @@
+import { createReactComponent } from './react-component-lib';
+import { defineCustomElements } from 'stencil-library/loader';
+defineCustomElements();
+export const MyComponent = createReactComponent('my-component');
+//# sourceMappingURL=index.js.map
